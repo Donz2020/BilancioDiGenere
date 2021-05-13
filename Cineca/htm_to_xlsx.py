66372935bbb7f699e502b6b9d67b4948e1972c26
Donz2020 <donoval.candolfi@studenti.unicam.it>
2021-05-13 11:56:12 +0200

    fix

--- Cineca/htm_to_xlsx.py
+++ Cineca/htm_to_xlsx.py
@@ -1,7 +1,7 @@
 from html2excel import ExcelParser
 
-input_file = 'C:/Users/Sviluppo-pc/temp.htm'
-output_file = 'C:/Users/Sviluppo-pc/temp_export.xlsx'
+input_file = 'C:/Users/dony9/temp.htm'
+output_file = 'C:/Users/dony9/temp_export.xlsx'
 
 parser = ExcelParser(input_file)
 parser.to_excel(output_file)

2bf64180424eef606b578a789d6c31a1f29bfc16
Donz2020 <donoval.candolfi@studenti.unicam.it>
2021-05-13 11:44:32 +0200

    added main job

--- Cineca/donoval/htm_to_xlsx.py
+++
@@ -1,7 +0,0 @@
-from html2excel import ExcelParser
-
-input_file = 'C:/Users/Sviluppo-pc/temp.htm'
-output_file = 'C:/Users/Sviluppo-pc/temp_export.xlsx'
-
-parser = ExcelParser(input_file)
-parser.to_excel(output_file)
